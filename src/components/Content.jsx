fffa4ac2ea88f1911ccec0b35a1ee525a11bf94f
eeeboooooy <zheka.mtb@gmail.com>
2020-07-25 02:50:35 +0800

    splitting css into modules

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Content.module.css';
+
+const Content = () => {
+    return(
+        <div className={s.mcontent}>
+        Main content
+        </div>
+    );
+}
+export default Content;
