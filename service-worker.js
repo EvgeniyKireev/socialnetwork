aecbc6371ca7e172111df060e400d26e2b45d28f
eeeboooooy <zheka.mtb@gmail.com>
2020-10-11 22:13:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/socialnetwork/precache-manifest.216f554ef2e9c4beeb938ce6e80bd879.js"
+  "/socialnetwork/precache-manifest.bcdbf441ab5af89a1e3f914627fd5523.js"
 );
 
 self.addEventListener('message', (event) => {
